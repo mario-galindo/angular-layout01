ce16adbe42bbd112c2df1f9c451dfc50ba816843
mario-galindo <mgalindo9410@gmail.com>
2021-05-20 16:45:59 -0600

    fetching user detail

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -10,4 +10,14 @@ export class DataService {
   getUsers() {
     return this.http.get('https://jsonplaceholder.typicode.com/users');
   }
+
+  getUser(userId) {
+    return this.http.get(
+      'https://jsonplaceholder.typicode.com/users/' + userId
+    );
+  }
+
+  getPosts() {
+    return this.http.get('https://jsonplaceholder.typicode.com/posts');
+  }
 }
