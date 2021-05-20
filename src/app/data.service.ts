31c465d3a91c6760e2a472c9104073516cd73dff
mario-galindo <mgalindo9410@gmail.com>
2021-05-20 10:11:00 -0600

    fetching users

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class DataService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  getUsers() {
+    return this.http.get('https://jsonplaceholder.typicode.com/users');
+  }
 }
